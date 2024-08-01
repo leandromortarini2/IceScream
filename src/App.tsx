62760129ce3de594301b3285c956ccde2b882e5e
leandro mortarini <leandromortarini2@gmail.com>
2024-08-01 18:32:57 -0300

    feat:carrusel

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import { Home } from "./view/Home/Home";
 function App() {
   return (
     <>
-    <Home/>
-      <div className="w-full min-h-screen bg-yellow-200 flex justify-center items-center">
-        <h1 className="text-6xl font-bold">IceScream</h1>
-      </div>
+      <Home />
     </>
   );
 }
--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
+import Example from "../../Components/Exemple/Exemple";
 
 export const Home: React.FC = () => {
   return (
-    <div className='w-full min-h-screen flex flex-col justify-evenly items-center'>
-        <div className='w-full min-h-[200px] sm:w-[70%] md:w-[80%] lg:w-[90%] lg:min-h-[300px] xl:h-[400px] bg-amber-400 mb-3'>
-            banner
+    <div className="w-full min-h-screen flex flex-col justify-evenly items-center">
+      <div className="w-full min-h-[200px] sm:w-[70%] md:w-[80%] lg:w-[90%] lg:min-h-[300px] xl:h-[400px] bg-amber-400 mb-3">
+        banner
+      </div>
+      <div className="w-full h-[300px] xl:h-[400px] flex flex-col md:flex-row justify-evenly items-center mb-5">
+        <div className="w-full md:w-1/3  md:h-[300px] lg:h-full bg-red-400  m-2 flex justify-center items-center">
+          <Example />
         </div>
-        <div className='w-full h-[300px] xl:h-[400px] flex flex-col md:flex-row justify-evenly items-center'>
-            <div className='w-full md:w-[30%] h-[200px] md:h-[300px] lg:h-full bg-red-400 mb-5 lg:mb-0'>carrusel 1</div>
-            <div className='w-full md:w-[30%] h-[200px] md:h-[300px] lg:h-full bg-red-400 mb-5 lg:mb-0'>carrusel 2</div>
+        <div className="w-full md:w-1/3  md:h-[300px] lg:h-full bg-red-400   flex justify-center items-center">
+          <Example />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
