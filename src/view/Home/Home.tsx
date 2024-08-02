6cbd41d7485d0d2ada0c39184ef7ac70497dc866
leandro mortarini <leandromortarini2@gmail.com>
2024-08-02 16:10:05 -0300

    style:estilos de home y navbar

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Home: React.FC = () => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-violeta  ">
-      <div className="w-full h-72  md:mt-20 overflow-hidden flex items-center justify-center ">
+      <div className="w-full h-72   overflow-hidden flex items-center justify-center ">
         <img
-          src="https://i.ibb.co/7t7Jh1G/Diseno-sin-titulo-4.png"
+          src="https://i.ibb.co/QF77dsK/Diseno-sin-titulo-4.png"
           alt=""
-          className="w-full relative top-24"
+          className="w-full relative top-20"
         />
       </div>
     </div>
