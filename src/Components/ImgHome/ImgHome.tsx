375c75cd99c382093f6752c8bf1314ed921390da
Agustina <agustinaayelensosa@gmail.com>
2024-08-03 18:20:01 -0300

    style: imgHome

--- src/Components/ImgHome/ImgHome.tsx
+++ src/Components/ImgHome/ImgHome.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const ImgHome: React.FC = () => {
   return (
-    <img src="https://i.ibb.co/FDHFSvZ/Cuadrado-Photoroom.png" alt="" className='h-full '/>
+    <img src="https://i.ibb.co/FDHFSvZ/Cuadrado-Photoroom.png" alt="" className='h-2/3 md:h-full '/>
   )
 }
