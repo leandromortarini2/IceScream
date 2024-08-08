534bb345370f77828005f36b3ec765d1837d141a
Agustina <agustinaayelensosa@gmail.com>
2024-08-08 17:09:16 -0300

    style: estilos al carrito, agregue img de anillado

--- src/Components/ShoppingCart/ShoppingCart.tsx
+++ src/Components/ShoppingCart/ShoppingCart.tsx
@@ -16,7 +16,10 @@ export const ShoppingCart: React.FC = () => {
 
   return (
     <div className='w-full min-h-screen z-40  flex justify-end items-start'>
-        <div className='w-full max-h-screen md:w-[350px] md:max-h-[450px] lg:max-h-[500px]  flex flex-col justify-start items-start bg-[#f5f8a1] overflow-y-auto cart-scrollbar  shadow-2xl'>
+        <div className='w-full max-h-screen sm:w-[300px] lg:w-[400px] sm:max-h-96 lg:max-h-96  flex flex-col justify-start items-start bg-[#f5f8a1] overflow-y-auto cart-scrollbar  shadow-xl shadow-PrimarioOscuro rounded sm:mr-5'>
+            <div className='w-full h-[50px] flex justify-center items-center '>
+                <img src="https://i.ibb.co/9w5V4DG/Dise-o-sin-t-tulo-3-Photoroom.png" alt="" />
+            </div>
             {
                 dataState ? (
                     dataState.map((e) => {
@@ -24,14 +27,14 @@ export const ShoppingCart: React.FC = () => {
                             <>
                             <div key={e.id} className='w-full max-h-screen flex justify-center items-center borderB p-5 '>
                                 <div className='w-[85%] flex flex-col justify-center items-center'>
-                                    <h3 className='text-center'><strong className='text-[#2b2d42] text-sm md:text-lg lg:text-xl'>{e.name}</strong>  <span className=' text-[#40425e]'>${e.price}</span></h3>
-                                    <p  className='text-[#2b2d42] text-center text-sm md:text-lg lg:text-xl'>
+                                    <h3 className='text-center'><strong className='text-[#2b2d42] text-sm md:text-md lg:text-xl'>{e.name}</strong>  <span className=' text-[#40425e]'>${e.price}</span></h3>
+                                    <p  className='text-[#2b2d42] text-center text-sm md:text-md lg:text-xl'>
                                         Sabores: {e.flavors.join(', ')}
                                     </p>
-                                    <p className='text-[#2b2d42] text-center text-sm md:text-lg lg:text-xl'>
+                                    <p className='text-[#2b2d42] text-center text-sm md:text-md lg:text-xl'>
                                     Salsas: {e.dip.join(', ')}
                                     </p>
-                                    <p className=' text-[#2b2d42] text-center text-sm md:text-lg lg:text-xl'>
+                                    <p className=' text-[#2b2d42] text-center text-sm md:text-md lg:text-xl'>
                                         Topping: {e.topping.join(', ')}
                                     </p>
                                 </div>
